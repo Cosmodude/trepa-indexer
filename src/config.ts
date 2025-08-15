e08d25382536ba50348364bb71161fda8ba0b587
Cosmodude <vladislav.lenskii@gmail.com>
2025-08-15 13:35:55 +0900

    fix: config + add docker yaml

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
-import 'dotenv/config'
+import { config as dotenvConfig } from 'dotenv'
+
+dotenvConfig()
 
 export const config = {
   database: {
-    url: process.env.DB_URL || 'postgresql://postgres:password@localhost:5433/postgres',
+    url: process.env.DB_URL || 'postgresql://postgres:password@localhost:5433/trepa',
   },
   solana: {
     rpcUrl: process.env.SOLANA_RPC_URL || 'https://devnet.helius-rpc.com/?api-key=53c70481-6c85-4803-8b66-ba1c134cb6d8',
