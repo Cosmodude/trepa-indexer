84e3186965d7ebb66912a2acd58ccbc2e7b1000b
Cosmodude <vladislav.lenskii@gmail.com>
2025-08-21 21:20:25 +0900

    fix: latest block height

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import {
 } from "./model"
 import {config} from './config'
 
-const START_BLOCK_HEIGHT = 390_307_340
+const START_BLOCK_HEIGHT = 390_557_321
 
 const dataSource = new DataSourceBuilder()
     //.setGateway('https://v2.archive.subsquid.io/network/solana-mainnet')
