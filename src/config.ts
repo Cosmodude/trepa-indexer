4aab1f5b36d8e35cbc7dea7fc08eb02cce72b15f
Cosmodude <vladislav.lenskii@gmail.com>
2025-08-21 13:43:32 +0900

    upd: prestart script + env naming + package

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenvConfig()
 
 export const config = {
   database: {
-    url: process.env.DB_URL || 'postgresql://postgres:postgres@localhost:5433/trepa',
+    url: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5433/trepa',
   },
   solana: {
     rpcUrl: process.env.SOLANA_RPC_URL || 'https://devnet.helius-rpc.com/?api-key=53c70481-6c85-4803-8b66-ba1c134cb6d8',
