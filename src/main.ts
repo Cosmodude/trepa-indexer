6fd927c6dfe7bd186906d79a5aab031058405529
Cosmodude <vladislav.lenskii@gmail.com>
2025-10-08 16:50:16 +0900

    start block update

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ import {
 } from "./model"
 import {config} from './config'
 
-const START_BLOCK_HEIGHT = 391_337_790
+const START_BLOCK_HEIGHT = 399_335_925
 
 const dataSource = new DataSourceBuilder()
     //.setGateway('https://v2.archive.subsquid.io/network/solana-mainnet')
