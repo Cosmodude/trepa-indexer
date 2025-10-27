a9ef19ea2999cfded41529394472afc8c8d9bb74
Cosmodude <vladislav.lenskii@gmail.com>
2025-10-27 18:52:23 +0900

    add: prettier + lint

---
+++ .eslintrc.js
@@ -0,0 +1,84 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', 'node_modules'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal'],
+          ['parent', 'sibling', 'index'],
+        ],
+        pathGroups: [
+          {
+            pattern: '@/libs/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/api/common/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/api/auth/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/api/modules/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/api/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/processor/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        distinctGroup: false,
+      },
+    ],
+    'import/no-duplicates': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+  },
+};
