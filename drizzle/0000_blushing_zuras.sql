87841f5db71815d804e1db23909f3518d267286c
Leon Meka <leonmeka@gmail.com>
2025-11-11 21:55:47 +0100

    feat: add new SQL schema for claims, hot blocks, hot change logs, predictions, and statuses with appropriate constraints and indexes

--- drizzle/0000_blushing_zuras.sql
+++ drizzle/0000_blushing_zuras.sql
@@ -1,27 +1,33 @@
 CREATE TABLE "claims" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT current_timestamp NOT NULL,
+	"updated_at" timestamp DEFAULT current_timestamp NOT NULL,
 	"reward_id" uuid,
 	"user_wallet_address" varchar(44) NOT NULL,
 	"prediction_account" varchar(44) NOT NULL,
 	"amount" bigint NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "hot_block" (
+CREATE TABLE "hot_blocks" (
 	"height" integer PRIMARY KEY NOT NULL,
-	"hash" text NOT NULL
+	"hash" text NOT NULL,
+	"created_at" timestamp DEFAULT current_timestamp NOT NULL,
+	"updated_at" timestamp DEFAULT current_timestamp NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "hot_change_log" (
+CREATE TABLE "hot_change_logs" (
 	"block_height" integer NOT NULL,
 	"index" integer NOT NULL,
 	"change" jsonb NOT NULL,
-	CONSTRAINT "hot_change_log_block_height_index_pk" PRIMARY KEY("block_height","index")
+	"created_at" timestamp DEFAULT current_timestamp NOT NULL,
+	"updated_at" timestamp DEFAULT current_timestamp NOT NULL,
+	CONSTRAINT "hot_change_logs_block_height_index_pk" PRIMARY KEY("block_height","index")
 );
 --> statement-breakpoint
 CREATE TABLE "predictions" (
 	"id" varchar PRIMARY KEY NOT NULL,
+	"created_at" timestamp DEFAULT current_timestamp NOT NULL,
+	"updated_at" timestamp DEFAULT current_timestamp NOT NULL,
 	"transaction_signature" text NOT NULL,
 	"timestamp" timestamp NOT NULL,
 	"pool_account" varchar(44) NOT NULL,
@@ -33,14 +39,16 @@ CREATE TABLE "predictions" (
 	"is_fee_payer" boolean NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "status" (
+CREATE TABLE "statuses" (
 	"id" integer PRIMARY KEY DEFAULT 0 NOT NULL,
 	"height" integer NOT NULL,
 	"hash" text NOT NULL,
-	"nonce" integer DEFAULT 0 NOT NULL
+	"nonce" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp DEFAULT current_timestamp NOT NULL,
+	"updated_at" timestamp DEFAULT current_timestamp NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "hot_change_log" ADD CONSTRAINT "hot_change_log_block_height_hot_block_height_fk" FOREIGN KEY ("block_height") REFERENCES "public"."hot_block"("height") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "hot_change_logs" ADD CONSTRAINT "hot_change_logs_block_height_hot_blocks_height_fk" FOREIGN KEY ("block_height") REFERENCES "public"."hot_blocks"("height") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "idx_claims_prediction_user" ON "claims" USING btree ("prediction_account","user_wallet_address");--> statement-breakpoint
 CREATE INDEX "idx_claims_user_wallet" ON "claims" USING btree ("user_wallet_address");--> statement-breakpoint
 CREATE INDEX "idx_claims_created_at" ON "claims" USING btree ("created_at");
